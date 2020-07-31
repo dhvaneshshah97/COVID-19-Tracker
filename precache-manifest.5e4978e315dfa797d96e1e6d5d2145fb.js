dd4f146a76e8cfed579c719362f9f4e96b5ea197
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-07-30 21:26:20 -0700

    Updates

---
+++ precache-manifest.5e4978e315dfa797d96e1e6d5d2145fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f596435bf4351ffd3697bf60db3705b6",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "746bd6c12da6b7aa173c",
+    "url": "/COVID-19-Tracker/static/css/main.e8a20286.chunk.css"
+  },
+  {
+    "revision": "921a08ed8d7d0c4d8448",
+    "url": "/COVID-19-Tracker/static/js/2.c150e34b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.c150e34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746bd6c12da6b7aa173c",
+    "url": "/COVID-19-Tracker/static/js/main.4a6b2857.chunk.js"
+  },
+  {
+    "revision": "cada4fdc3a4cf6e5d769",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.6f0900ac.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/covid.d7265326.png"
+  }
+]);
